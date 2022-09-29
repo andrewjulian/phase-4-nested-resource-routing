51d948fa69294e926a9f3b09cfce7e26ad729b90
+ <+>
2022-09-29 07:21:21 -0600

    commit tree c220a36b9b4be6b21659d759415aa38d96c44fb9

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,4 +1,4 @@
-class DogHousesController < ApplicationController
+lass DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
